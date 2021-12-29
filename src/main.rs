e586e651c95da725d94a6ba861dd650857f32486
Vanilla <osu_Vanilla@126.com>
2021-12-29 20:46:49 +0800

    [+] src/main.rs: A simple implement, read .steam_secret as file.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::fs;
+
+const STEAM_SECRET: &str = ".steam_secret";
+
 fn main() {
-    println!("Hello, world!");
+    let mut home_dir_buf = dirs::home_dir()
+        .expect("Error while getting home_dir. ");
+    home_dir_buf.push(STEAM_SECRET);
+
+    let error_msg = format!("Error while reading file {:?}. ", home_dir_buf);
+    let steam_secret = fs::read_to_string(home_dir_buf)
+        .expect(error_msg.as_str()).trim().to_string();
+
+    let sg_code = steam_guard::from_secret(&steam_secret)
+        .unwrap_or_else(|err| { panic!("{}", err) });
+    let expire_sec = steam_guard::expires_in_sec();
+
+    println!("Steam Guard Code: {}, expire in {} s. ", sg_code, expire_sec);
 }
