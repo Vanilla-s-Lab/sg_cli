afacf89abe5e3ffdaf2a8d79f8d592362e3e4ca5
Vanilla <osu_Vanilla@126.com>
2021-12-30 14:38:24 +0800

    [+] tests/steam_guard.rs: Add helper fn to read & parse config.

--- tests/steam_guard.rs
+++ tests/steam_guard.rs
@@ -6,11 +6,14 @@ use sg_cli::Config;
 
 #[test]
 fn fake_steam_guard() {
-    let fake_secret: Config = fs::read_to_string("./example/sg_cli.toml")
-        .unwrap().pipe(|it| { toml::from_str(it.as_str()) }).unwrap();
-
+    let fake_config = read_config();
     let sg_result =
-        steam_guard::from_secret(fake_secret.sg_cli.secret.as_str());
+        steam_guard::from_secret(fake_config.sg_cli.secret.as_str());
 
     assert!(sg_result.is_err());
+}
+
+fn read_config() -> Config {
+    fs::read_to_string("./example/sg_cli.toml").unwrap()
+        .pipe(|it| { toml::from_str(it.as_str()) }).unwrap()
 }
