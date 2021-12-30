7000ce0edef08898bc1e0c7e2ca85521a59eb15f
Vanilla <osu_Vanilla@126.com>
2021-12-30 17:46:53 +0800

    [-] nix-shell/codecov.nix: Remove glibc deps, add -v for more details.

--- nix-shell/codecov.nix
+++ nix-shell/codecov.nix
@@ -1,9 +1,9 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [ cargo-tarpaulin cargo glibc ];
+  nativeBuildInputs = with pkgs; [ cargo-tarpaulin cargo ];
 
   # https://github.com/xd009642/tarpaulin#travis-ci-and-coverage-sites
-  shellHook = "exec cargo tarpaulin --out Xml";
+  shellHook = "exec cargo tarpaulin -v --out Xml";
 
   # https://nixos.wiki/wiki/Development_environment_with_nix-shell#Troubleshooting
   NIX_ENFORCE_PURITY = 0;
